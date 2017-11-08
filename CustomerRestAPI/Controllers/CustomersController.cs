8e9f6b0a8d390d504786afc57adf86d2f4f8f996
edijavi <edilata@msn.com>
2017-11-08 22:01:58 +0100

    Creation new project of .net core web aplication, testing postman get and post

---
+++ CustomerRestAPI/Controllers/CustomersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CustomerAppBLL;
+using CustomerAppBLL.BusinessObjects;
+
+namespace CustomerRestAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class CustomersController : Controller
+    {
+        BLLFacade facade = new BLLFacade();
+        // GET: api/Customers
+        [HttpGet]
+        public IEnumerable<CustomerBO> Get()
+        {
+            return facade.CustomerService.GetAll();
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}", Name = "Get")]
+        public CustomerBO Get(int id)
+        {
+            return facade.CustomerService.Get(id);
+        }
+        
+        // POST: api/Customers
+        [HttpPost]
+        public void Post([FromBody]CustomerBO cust)
+        {
+            facade.CustomerService.Create(cust);
+        }
+        
+        // PUT: api/Customers/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
